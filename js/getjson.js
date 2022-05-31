10f8e6dea00a813c4d6298520a1c095aa4dd73a4
iremmarz <73883518+iremmarz@users.noreply.github.com>
2022-05-31 23:06:16 +0200

    Update espacio header

--- js/getjson.js
+++ js/getjson.js
@@ -1,13 +1,13 @@
 /* jshint esversion: 8 */
 
 
-let libros = getJson("https://biblioteca-coll.netlify.app/data/libros.json");
+//let libros = getJson("https://biblioteca-coll.netlify.app/data/libros.json");
 //let peliculas = getJson("https://hollypedia.netlify.app/json/peliculas.json");
 //let peliculas = fetchJSON();
 //console.log(peliculas);
 //fetchJSON("https://hollypedia.netlify.app/json/peliculas.json").then(peliculas => result.json()).then(peliculas => console.log(peliculas));
 
-sessionStorage.setItem("jsonLibros",libros);
+
 
 function getJson(url){
   
@@ -28,8 +28,9 @@ function getJson(url){
   return aux;
 }
 
-async function fetchJSON() {
-    const response = await fetch('https://hollypedia.netlify.app/json/peliculas.json');
+async function fetchJSON1(url) {
+  console.log("fetch1");
+    const response = await fetch(url);
     if (!response.ok) {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
@@ -37,12 +38,29 @@ async function fetchJSON() {
     const data = response.json();
     return data;
 }
-fetchJSON().catch(error => {
+async function fetchJSON2(url) {
+  console.log("fetch2");
+    const response = await fetch(url);
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    }
+    const data = response.json();
+    return data;
+}
+fetchJSON2().catch(error => {
     error.message; // 'An error has occurred: 404'
 });
 
 async function fetchPelis(){
-  let peliculas = await fetchJSON();
-  sessionStorage.setItem("jsonPeliculas",JSON.stringify(peliculas));
-  //console.log(peliculas);
+  let peliculas = await fetchJSON1('https://hollypedia.netlify.app/json/peliculas.json');
+  sessionStorage.setItem("jsonPeliculas",JSON.parse(peliculas));
+  console.log(peliculas);
+}
+
+async function fetchLibros(){
+  let libros = await fetchJSON2('https://biblioteca-coll.netlify.app/data/libros.json');
+  console.log(libros);
+  sessionStorage.setItem("jsonLibros",JSON.parse(libros));
+  
 }
